60f4e5753e5dd345dfe6fd325f72908c2b8fea45
voidpuz <voidpointer07@gmail.com>
2025-05-08 20:06:44 +0500

    stage1: init custom user model

---
+++ accounts/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+
+from accounts.models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("id", "email", "phone_number", "first_name", "last_name", "is_active", "is_staff")
+    list_display_links = ("id", "email", "phone_number", "first_name", "last_name")
+    search_fields = ("email", "phone_number", "first_name", "last_name")
+    list_filter = ("is_active", "is_staff")
+
