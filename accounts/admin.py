4573a958ca41e1a63d4ccca351d6c74cec11692d
voidpuz <voidpointer07@gmail.com>
2025-05-14 15:10:30 +0500

    fix: User admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 from accounts.models import User, Cart, CartItem
 
@@ -8,11 +9,29 @@ class CartItemInline(admin.TabularInline):
     extra = 1
 
 @admin.register(User)
-class UserAdmin(admin.ModelAdmin):
+class UserAdmin(BaseUserAdmin):
     list_display = ("id", "email", "phone_number", "first_name", "last_name", "is_active", "is_staff")
     list_display_links = ("id", "email", "phone_number", "first_name", "last_name")
     search_fields = ("email", "phone_number", "first_name", "last_name")
     list_filter = ("is_active", "is_staff")
+    
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (('Personal info'), {'fields': ('first_name', 'last_name', 'phone_number', 'avatar', 'bio')}),
+        (('Permissions'), {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+    )
+    
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }), 
+    )
+    
+    ordering = ('email',)
+    filter_horizontal = ('groups', 'user_permissions',)
 
 
 @admin.register(Cart)
