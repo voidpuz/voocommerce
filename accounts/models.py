08c7829785f0b45a4184d49ce52fdf5a049afe51
voidpuz <voidpointer07@gmail.com>
2025-06-10 20:59:25 +0500

    translations

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.utils.translation import gettext_lazy as _
 
 from common.models import BaseModel
 from accounts.manager import UserManager
@@ -25,6 +26,10 @@ class User(AbstractBaseUser, PermissionsMixin, BaseModel):
 
     def __str__(self):
         return self.email
+    
+    class Meta:
+        verbose_name = _("User")
+        verbose_name_plural = _("Users")
     
 
 class Cart(BaseModel):
