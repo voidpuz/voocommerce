cdd2e0bc2dc35bc5d5d88c5cde034b57239671da
voidpuz <voidpointer07@gmail.com>
2025-06-12 21:06:25 +0500

    add: celery and redis

--- core/settings.py
+++ core/settings.py
@@ -308,3 +308,9 @@ LANGUAGES = [
 ]
 
 LOCALE_PATHS = [BASE_DIR / "locale"]
+
+
+# CELERY settings
+
+CELERY_BROKER_URL = 'redis://localhost:6379/0'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/1'
